e80c3526d41b848fca7b15fc9bdd1ffb883b8a25
kikurii <wataru.kikuchi@notari.co.jp>
2016-12-05 15:07:41 +0900

    Initial Commit

---
+++ Mailer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mailer
+//
+//  Created by kikuchi wataru on 2016/12/05.
+//  Copyright © 2016年 kikuchi wataru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
