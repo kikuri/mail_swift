1943a32a9dd89491cd0ee87da3f7c43bf058b0e2
kikurii <wataru.kikuchi@notari.co.jp>
2016-12-06 12:02:19 +0900

    Second commit

--- Mailer/ViewController.swift
+++ Mailer/ViewController.swift
@@ -7,19 +7,36 @@
 //
 
 import UIKit
+import MessageUI
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController,MFMailComposeViewControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBOutlet weak var mailTapped: UIButton!
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
-}
+    
+    
+    @IBAction func mailTapped(_ sender: Any) {
+        //メールが送信できるかどうかの確認は必ず必要
+        if MFMailComposeViewController.canSendMail() {
+            let e = MFMailComposeViewController()
+            e.mailComposeDelegate = self //delegateの設定
+            e.setToRecipients(["test@swift-study.com"]) //宛先アドレス
+            e.setCcRecipients(["cc@swift-study.com"]) //CCアドレス
+            e.setBccRecipients(["bcc@swift-study.com"]) //BCCアドレス
+            e.setSubject("テストサブジェクトです") //サブジェクト
+            e.setMessageBody("テスト本文です", isHTML: false) //メール本文
+            present(e, animated: true, completion: nil) //メール作成画面呼び出し
+        } else {
+            print("送信できません")
+        }
+    }
+   
+    
+   }
 
